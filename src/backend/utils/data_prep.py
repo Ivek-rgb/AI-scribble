c3d0cab1120a0270e8742ceacd52e5c5cac2c3d5
Ivek <ivan.bencic6@gmail.com>
2025-01-10 21:44:09 +0100

    feat: added data augmenter for models

--- src/backend/utils/data_prep.py
+++ src/backend/utils/data_prep.py
@@ -1,9 +1,11 @@
 
-import numpy as np
 import os
-from PIL import Image
 import csv
 import math
+import base64
+import numpy as np
+from PIL import Image, ImageEnhance, ImageOps
+from io import BytesIO
 
 # 28*28 - image resolution, both for numbers and doodles   
 
@@ -13,7 +15,7 @@ class DataLoader:
         self.path = path
         self.data = None
 
-    def load_data_npy(self, dir : str, reshape_to_2828 : bool = False, append = True): 
+    def load_data_npy(self, reshape_to_2828 : bool = False, append = True): 
         data = []
         files = [file for file in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, file)) and os.path.splitext(file)[1] == '.npy']
         for file in files:
@@ -32,23 +34,7 @@ class DataLoader:
             self.data = self.data if self.data != None else [] + data
         else: self.data = data
         return self.data
-    
-    def interpolate(self,  data : list): 
-        return np.interp(data, [-1, 1], [0, 255])
 
-    def convert_arr_to_img(self, array): 
-        new_arr = np.reshape(array, (28, 28))
-        return Image.fromarray(new_arr)
-
-    def visualize_array(self, array): 
-        img = self.convert_arr_to_img(array)
-        img.show(title="image_repo")
-
-    def deserialize_numbers_data(self, row : str):
-        number_label, *data = map( lambda x : float(x), row.split(','))
-        return [number_label, data]    
-
-    #TODO: create interface for data or atleast object to handle it better
     def load_data_csv_nums(self, limit = math.inf, append = True):
         with open(self.path, newline='', encoding='utf-8') as training_data: 
             reader = csv.reader(training_data, delimiter='\t')
@@ -73,6 +59,54 @@ class DataLoader:
         for cell in self.data:
             cell["data"] = list(map(func, cell["data"]))
         return self.data
+    
+    @staticmethod
+    def fix_b64_padding(b64_coded_string): 
+        return b64_coded_string + ('=' * (4 - len(b64_coded_string) % 4)) 
+    
+    @staticmethod
+    def image_base64_decode(image_encoded) -> str: 
+        image_encoded = DataLoader.fix_b64_padding(image_encoded)
+        return base64.b64decode(image_encoded)
+    
+    @staticmethod
+    def load_resize_image(image_decoded, resize_dims : tuple[int] = (28, 28)) -> np.ndarray: 
+        image_data = Image.open(BytesIO(image_decoded)).convert('L')
+        image_data = ImageOps.invert(image_data)        
+        enhancer = ImageEnhance.Brightness(image_data)
+        image_data = enhancer.enhance(50)
+        if resize_dims != None: 
+            image_data = image_data.resize(resize_dims)
+        np_rep = np.array([np.array(image_data)]) 
+        return  np_rep
+    
+    @staticmethod
+    def b64_img_to_nparr(image_encoded): 
+        return DataLoader.load_resize_image(DataLoader.image_base64_decode(image_encoded))
+        
+    @staticmethod        
+    def interpolate(data : list): 
+        return np.interp(data, [-1, 1], [0, 255])
+    
+    @staticmethod
+    def numpy_array_mapper(numpy_array : np.ndarray, transformation_func): 
+        vectorized_function = np.vectorize(transformation_func)
+        return vectorized_function(numpy_array)
+
+    @staticmethod
+    def convert_arr_to_img(array): 
+        new_arr = np.reshape(array, (28, 28))
+        return Image.fromarray(new_arr)
+
+    @staticmethod
+    def visualize_array(array): 
+        img = DataLoader.convert_arr_to_img(array)
+        img.show(title="image_repo")
+    
+    @staticmethod
+    def deserialize_numbers_data(row : str):
+        number_label, *data = map( lambda x : float(x), row.split(','))
+        return [number_label, data] 
 
     def set_labels(self, data, label): 
         pass 
--- src/backend/utils/main.py
+++ src/backend/utils/main.py
@@ -1,5 +1,5 @@
 import data_prep
-from neural_networks import neural_net as nn, test_train_data as tt_data, neural_models as nm
+from neural_networks import neural_net as nn, neural_models as nm
 from functools import reduce
 import numpy as np
 import tensorflow as tf
@@ -11,40 +11,27 @@ def main():
     #network = nn.ToyNeuralNetwork(784, (128,), 10, 0.1, 0)
     neural_model = nm.NeuralModels() 
     #neural_model.mlp_model((784, 128, 64, 10), ["relu", "relu", "softmax"], [0.3])
-    neural_model.conv2D_model((28, 28, 1), (32, 64), [(3,3)], (2,2), ["relu", "relu", "relu", "softmax"], (512, 10), [0.25, 0.5]) 
+    neural_model.conv2D_model((28, 28, 1), (32, 64), [(2,2)], (2,2), ["relu", "relu", "relu", "softmax"], (512, 10), [0.25, 0.5]) 
+    neural_model.set_augmentation_datagen(5, 0, 0, 0, [0.5, 1]) # augmentator only rotates images slightly and zooms in on some 
     neural_model.model_compile()
 
-    npy_loader = data_prep.DataLoader('../data/training-set/doodles_data/')
+    loader_train = data_prep.DataLoader('../../../data/training-set/number_data/mnist_train.csv') 
+    loader_test = data_prep.DataLoader('../../../data/training-set/number_data/mnist_test.csv') 
     
-    loaded_data = npy_loader.load_data_npy(True) # resizeamo na 28, 28, 1 zbog korištenja konvolucijske mreže  
-
-    print(len(loaded_data)) # broj dataseta odnosno kategorija crteža (jabuka, lav), koliko .npy fileova imaš 
-    print(len(loaded_data[0])) # broj primjeraka unutar dataseta za jednu kategoriju crteža (jabuka, lav), koliko primjera crteža ima za prvu kategoriju 
-    for i in range(0, len(loaded_data)):
-        print(loaded_data[i]["label"])
-    loaded_data = np.array(loaded_data)
-    print(loaded_data)
-    
-    return # ovo kasnije je za brojeve pa ću samo to omittat preko return-a 
-
-    loader_train = data_prep.DataLoader('../data/training-set/number_data/mnist_train.csv') 
-    loader_test = data_prep.DataLoader('../data/training-set/number_data/mnist_test.csv') 
-    
-    loader_train.load_data_csv_nums(15000)
-    loader_test.load_data_csv_nums(1000)
+    loader_train.load_data_csv_nums()
+    loader_test.load_data_csv_nums()
     #test_data = loader.load_data_csv_nums('../data/training-set/number_data/mnist_test.csv', 1000)
     
     #print("prije")
-    train_data = loader_train.custom_mapper(lambda x : x / 255)
+    train_data = loader_train.custom_mapper(lambda x: x / 255)
     test_data = loader_test.custom_mapper(lambda x: x / 255)
-        
+    
     #print("posli")
     #print(test_data[0])
 
     #loader.visualize_array(train_data[0]["data"])
     
     # train the network 
-    
     train_x = list(map(lambda x: x["data"], train_data))
     train_y = list(map(lambda x: x["label"], train_data))
     
@@ -52,7 +39,8 @@ def main():
     train_x = np.array(list(map(lambda x: np.array(x).reshape(28, 28, 1), train_x)))
     train_y = np.array(train_y)
 
-    neural_model.fit(train_x, train_y, epochs=10, batch_size=1000, verbose=0)     
+    neural_model.fit(train_x, train_y, epochs=100, batch_size=128, verbose=1) 
+    print("Training completed")    
     
     #for row in train_data:
         #network.train(list(map(lambda x : x / 255, row["data"])), row["label"])
@@ -60,16 +48,14 @@ def main():
     err_count = 0
     
     for row in test_data:
-        row_data = row["data"]
-        row_data = np.array(row_data).reshape(-1, 28, 28, 1)
+        row_data = np.array(row["data"]).reshape(-1, 28, 28, 1)
         output_list = neural_model.predict(row_data, verbose=0).tolist()[0]
         #output_list = network.predict(list(map(lambda x : x / 255, row["data"]))).reshape(1, -1).tolist()[0]
-        if output_list.index(max(output_list)) != row["label"].index(max(row["label"])): 
-            err_count += 1
+        err_count += output_list.index(max(output_list)) != row["label"].index(max(row["label"]))
                         
     print((err_count / len(loader_test.data)) * 100, "% wrong guesses")
     
-    neural_model.save_as_model('./models/', 'conv2D_number_model_1')
+    neural_model.save_as_model('../models/', 'conv2D_number_model_1')
     
 if __name__ == '__main__': 
     main()
